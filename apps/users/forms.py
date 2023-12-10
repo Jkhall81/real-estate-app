18d92d1ecf11ed38c18736500b8642ea124595f0
Jkhall81 <jason.kei.hall@gmail.com>
2023-12-10 11:04:10 -0700

    added customer user model, set up django admin, and... what else did i do... migrated db.  currently using a postgres db set up on my local machine.

---
+++ apps/users/forms.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from .models import User
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm):
+        mode = User
+        fields = ['email', 'username', 'first_name', 'last_name']
+        error_class = 'error'
+        
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        mode = User
+        fields = ['email', 'username', 'first_name', 'last_name']
+        error_class = 'error'
